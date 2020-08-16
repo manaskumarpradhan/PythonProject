81ab42bcdee6af2301d751c05af7a069ae924349
manaskumarpradhan <manaspradhan.kumar@gmail.com>
2020-08-16 19:54:59 +0530

    Initial commit

---
+++ mymainpackage/somemainscript.py
@@ -0,0 +1,2 @@
+def report_main():
+    print("hey i am in some main_script.py in mainpackag")
---
+++ myprogram.py
@@ -0,0 +1,5 @@
+from mymainpackage import somemainscript
+from mymainpackage.subpackage import mysubscript
+
+somemainscript.report_main()
+mysubscript.sub_report()
